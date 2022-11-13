aeb3be80e02bb267782f07d0bc433561c6fd9788
Ivan Filatov <nymezide@gmail.com>
2022-11-13 22:14:51 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.AddContext<MyJsonDtoContext>();
+});
+
+//builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
+//{
+//    options.JsonSerializerOptions.AddContext<MyJsonDtoContext>();
+//});
+
+
+var app = builder.Build();
+
+app.MapPost("/test", (MyJsonDto dto) =>
+{
+    return dto;
+});
+
+app.Run();
+
+
+
+public class MyJsonDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
+[JsonSerializable(typeof(MyJsonDto))]
+public partial class MyJsonDtoContext : JsonSerializerContext { }
